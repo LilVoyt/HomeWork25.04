76a413c05635e43da78abb5452cf2fbdd5ac9825
Voyt <olegvoyt6@gmail.com>
2024-04-25 13:02:35 +0300

    Add project files.

---
+++ Interfaces/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            UserArray arr1 = new UserArray();
+            arr1.fillRand(10);
+            Console.WriteLine(arr1);
+
+            Console.WriteLine($"Number of less elements: {arr1.NumberOfLess(50)}");
+            Console.WriteLine(arr1.CountDistinct());
+            Console.WriteLine(arr1.EqualToValue(5));
+        }
+    }
+}
---
+++ Interfaces/UserArray.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class UserArray : ICalc, ICalc2
+    {
+        public List<int> Arr { get; set; }
+
+        public UserArray()
+        {
+            Arr = new List<int>();
+        }
+
+        public void fillRand(int size)
+        {
+            Random random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                Arr.Add(random.Next(0, 20));
+            }
+        }
+
+        public int NumberOfLess(int value)
+        {
+            int count = 0;
+            foreach(int numbers in Arr)
+            {
+                if(numbers < value)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public int NumberOfGreater(int value)
+        {
+            int count = 0;
+            foreach (int numbers in Arr)
+            {
+                if (numbers > value)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach(int numbers in Arr)
+            {
+                stringBuilder.Append($"{numbers} ");
+            }
+            return $"[ {stringBuilder}]";
+        }
+
+        public int CountDistinct()
+        {
+            List<int> temp = new List<int>();
+            foreach(int numbers in Arr)
+            {
+                if (!(temp.Contains(numbers)))
+                {
+                    temp.Add(numbers);
+                }
+            }
+            return temp.Count;
+        }
+
+        public int EqualToValue(int valueToCompare)
+        {
+            int count = 0;
+            foreach(int numbers in Arr)
+            {
+                if(numbers == valueToCompare)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
